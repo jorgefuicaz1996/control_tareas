def264bbadb7177af22ec8963a50fc8e194f7b5c
Takumi Stryder <jorgefuicaz1996@gmail.com>
2020-10-23 14:16:04 -0300

    nuevas vistas programadas y rutas

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
 	path('', views.PrincipalView.as_view(), name = 'principal'),
 	path('panel', views.PanelView.as_view(), name = 'panel'),
 	path('crear-tarea', views.Crear_tareaView.as_view(), name = 'crear-tarea'),
+	path('asignar-tarea', views.MenuAsignacionTareasView.as_view(), name = 'menu-asignar-tarea'),
+	path('asignar-tarea/<int:tarea>', views.ListaResponsableView.as_view(), name = 'lista-responsable'),
+	path('asignar-tarea/<int:tarea>/<int:responsable>', views.asignar_responsable, name = 'asignar-responsable'),
+	path('mistareas', views.MisTareasView.as_view(), name = 'mis-tareas'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+from datetime import datetime, timedelta
 from .forms import *
+from django.db.models import Q
+from api.models import *
 
 # Create your views here.
 class PrincipalView(View):
@@ -10,23 +14,75 @@ class PrincipalView(View):
 	def get(self, request):
 		return render(request, self.template_name, self.context)
 
-class PanelView(View):
+class PanelView(LoginRequiredMixin, View):
 	template_name = 'panel.html'
 	context = {'title': 'Panel'}
 
 	def get(self, request):
+		funcionario = Funcionario.objects.get(email = request.user.username)
+		self.context['funcionario'] = funcionario
 		return render(request, self.template_name, self.context)
-		
-class Crear_tareaView(View):
+
+class Crear_tareaView(LoginRequiredMixin, View):
 	template_name = 'crear_tarea.html'
-	form = Crear_tareaForm()
+	form = CrearTareaForm()
 	context = {'title': 'Inicio de sesión', 'form': form, 'fail': False}
 
 	def get(self, request):
 		return render(request, self.template_name, self.context)
 
 	def post(self, request):
-		self.form = Crear_tareaForm(request.POST)
+		self.form = CrearTareaForm(request.POST)
 		if self.form.is_valid():
-			return render(request, self.template_name, self.context)
+			data = self.form.cleaned_data
+			Tarea.objects.create(
+				descripcion = data.get('descripcion'),
+				fecha_inicio = data.get('fecha_inicio'),
+				fecha_plazo = data.get('fecha_inicio') + timedelta(days = data.get('duracion_dias')),
+				duracion_dias = data.get('duracion_dias'),
+				estado = EstadoTarea.objects.get(descripcion = 'Creada')
+			)
+			return redirect('crear-tarea')
 		return render(request, self.template_name, self.context)
+
+class MenuAsignacionTareasView(View):
+	template_name = 'menu_asignar_tareas.html'
+	context = {'title': 'Asignación de Tareas'}
+
+	def get(self, request):
+		tareas = Tarea.objects.all()
+		self.context['tareas'] = tareas
+		return render(request, self.template_name, self.context)
+
+class ListaResponsableView(View):
+	template_name = 'lista_responsables.html'
+	context = {'title': 'Asignar Responsable'}
+
+	def get(self, request, tarea):
+		tarea = Tarea.objects.get(pk = tarea)
+		funcionarios = Funcionario.objects.exclude(pk__in = ResponsableTarea.objects.filter(tarea = tarea).values('funcionario'))
+		self.context['tarea'] = tarea
+		self.context['funcionarios'] = funcionarios
+		return render(request, self.template_name, self.context)
+
+def asignar_responsable(request, tarea, responsable):
+	ResponsableTarea.objects.create(
+		funcionario = Funcionario.objects.get(pk = responsable),
+		tarea = Tarea.objects.get(pk = tarea)
+	)
+	tarea_obj = Tarea.objects.get(pk = tarea)
+	if tarea_obj.estado != 'Asignada':
+		tarea_obj.estado = EstadoTarea.objects.get(descripcion = 'Asignada')
+		tarea_obj.save()
+	return redirect('lista-responsable', tarea)
+
+class MisTareasView(View):
+	template_name = 'mis_tareas.html'
+	context = {'title': 'Mis Tareas'}
+
+	def get(self, request):
+		funcionario = Funcionario.objects.get(email = request.user.username)
+		tareas = Tarea.objects.filter(responsables = funcionario)
+		self.context['tareas'] = tareas
+		return render(request, self.template_name, self.context)
+
