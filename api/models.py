f0aa190fb7be9666f367034b8a44d6851e32b1e8
Takumi Stryder <jorgefuicaz1996@gmail.com>
2020-11-06 15:12:26 -0300

    modelos integrados a oracle

--- api/models.py
+++ api/models.py
@@ -2,46 +2,75 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class RolDepto(models.Model):
+	id_rol = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 50)
+
+	class Meta:
+		db_table = 'rol_depto'
+		managed = False
+
+	def __str__(self):
+		return self.nombre
+
+class RolFuncionario(models.Model):
+	id_rol = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 40)
+
+	class Meta:
+		db_table = 'rol_funcionario'
+		managed = False
+
+	def __str__(self):
+		return self.nombre
+
+class EstadoTarea(models.Model):
+	id_estado = models.AutoField(primary_key = True)
+	descripcion = models.CharField(max_length = 20)
+
+	class Meta:
+		db_table = 'estado_tarea'
+		managed = False
+
+	def __str__(self):
+		return self.descripcion
+
 class Region(models.Model):
 	id_region = models.IntegerField(primary_key = True)
 	nombre = models.CharField(max_length = 50)
 
-	def __str__(self):
-		return self.nombre
-
 	class Meta:
 		db_table = 'region'
+		managed = False
+
+	def __str__(self):
+		return self.nombre
 
 class Comuna(models.Model):
 	id_comuna = models.IntegerField(primary_key = True)
 	nombre = models.CharField(max_length = 50)
-	region = models.ForeignKey(Region, on_delete = models.DO_NOTHING)
-
-	def __str__(self):
-		return self.nombre
+	region = models.ForeignKey(Region, on_delete = models.CASCADE, db_column = 'id_region')
 
 	class Meta:
 		db_table = 'comuna'
+		managed = False
+
+	def __str__(self):
+		return self.nombre
 
 class Empresa(models.Model):
 	id_empresa = models.AutoField(primary_key = True)
 	rut = models.CharField(max_length = 10, unique = True)
 	nombre = models.CharField(max_length = 50)
 	direccion = models.CharField(max_length = 70)
-	comuna = models.ForeignKey(Comuna, on_delete = models.DO_NOTHING)
+	comuna = models.ForeignKey(Comuna, on_delete = models.CASCADE, db_column = 'id_comuna')
 
 	class Meta:
 		db_table = 'empresa'
-
-class RolFuncionario(models.Model):
-	id_rol = models.AutoField(primary_key = True)
-	nombre = models.CharField(max_length = 20)
+		managed = False
 
 	def __str__(self):
 		return self.nombre
-
-	class Meta:
-		db_table = 'rol_funcionario'
 
 class Funcionario(models.Model):
 	id_funcionario = models.AutoField(primary_key = True)
@@ -53,60 +82,71 @@ class Funcionario(models.Model):
 	email = models.EmailField(max_length = 50)
 	telefono_movil = models.IntegerField()
 	telefono_fijo = models.IntegerField(null = True, blank = True)
-	comuna = models.ForeignKey(Comuna, on_delete = models.DO_NOTHING)
-	rol = models.ForeignKey(RolFuncionario, on_delete = models.DO_NOTHING)
-	empresa = models.ForeignKey(Empresa, on_delete = models.DO_NOTHING)
-	usuario = models.OneToOneField(User, on_delete = models.DO_NOTHING)
+	comuna = models.ForeignKey(Comuna, on_delete = models.CASCADE, db_column = 'id_comuna')
+	rol = models.ForeignKey(RolFuncionario, on_delete = models.CASCADE, db_column = 'id_rol')
+	empresa = models.ForeignKey(Empresa, on_delete = models.CASCADE, db_column = 'id_empresa')
+	usuario = models.OneToOneField(User, on_delete = models.CASCADE, db_column = 'user_id')
+
+	class Meta:
+		db_table = 'funcionario'
+		managed = False
 
 	def __str__(self):
 		return '{} {} {}'.format(self.nombres, self.ap_paterno, self.ap_materno)
 
-	class Meta:
-		db_table = 'funcionario'
+class Departamento(models.Model):
+	id_depto = models.AutoField(primary_key = True)
+	nombre_depto = models.CharField(max_length = 50)
+	rol = models.ForeignKey(RolDepto, on_delete = models.CASCADE, db_column = 'id_rol')
+	empresa = models.ForeignKey(Empresa, on_delete = models.CASCADE, db_column = 'id_empresa')
 
-class RolDepto(models.Model):
-	id_rol = models.AutoField(primary_key = True)
-	nombre = models.CharField(max_length = 50)
+	class Meta:
+		db_table = 'departamento'
+		managed = False
+
+	def __str__(self):
+		return self.nombre_depto
+
+class Funcion(models.Model):
+	id_funcion = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 30)
+	descripcion = models.CharField(max_length = 100)
+	depto = models.ForeignKey(Departamento, on_delete = models.CASCADE, db_column = 'id_depto')
+
+	class Meta:
+		db_table = 'funcion'
+		managed = False
 
 	def __str__(self):
 		return self.nombre
 
-	class Meta:
-		db_table = 'rol_depto'
-
-class EstadoTarea(models.Model):
-	id_estado = models.AutoField(primary_key = True)
-	descripcion = models.CharField(max_length = 20)
-
-	def __str__(self):
-		return self.descripcion
-
-	class Meta:
-		db_table = 'estado_tarea'
-
 class Tarea(models.Model):
 	id_tarea = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 30)
 	descripcion = models.CharField(max_length = 100)
 	fecha_inicio = models.DateField()
 	fecha_termino = models.DateField(null = True, blank = True)
 	fecha_plazo = models.DateField()
 	duracion_dias = models.IntegerField()
-	estado = models.ForeignKey(EstadoTarea, on_delete = models.DO_NOTHING)
+	estado = models.ForeignKey(EstadoTarea, on_delete = models.CASCADE, db_column = 'id_estado')
 	responsables = models.ManyToManyField(Funcionario, through = 'ResponsableTarea')
+
+	class Meta:
+		db_table = 'tarea'
+		managed = False
 
 	def __str__(self):
 		return self.descripcion
 
-	class Meta:
-		db_table = 'tarea'
-
 class ResponsableTarea(models.Model):
-	funcionario = models.ForeignKey(Funcionario, on_delete = models.DO_NOTHING)
-	tarea = models.ForeignKey(Tarea, on_delete = models.DO_NOTHING)
+	funcionario = models.ForeignKey(Funcionario, on_delete = models.CASCADE, db_column = 'id_funcionario')
+	tarea = models.ForeignKey(Tarea, on_delete = models.CASCADE, db_column = 'id_tarea')
+	justificativo = models.CharField(max_length = 50)
 	plazo_dias = models.IntegerField()
 
 	class Meta:
 		db_table = 'responsable_tarea'
 		constraints = [
-			models.UniqueConstraint(name = 'funcionario_tarea_UN', fields = ['funcionario', 'tarea'])
+			models.UniqueConstraint(name = 'funcionario_tarea_un', fields = ('funcionario', 'tarea'))
 		]
+		managed = False
